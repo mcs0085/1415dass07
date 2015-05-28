601813c00db6362706fc91e066efced8b8f8cfe9
mcs0085 <mcs0085@alu.ubu.es>
2015-05-28 18:31:14 +0200

    #1 Refactor Extract Method "introducirDatos" de "printDocument".

--- src/lanSimulation/Network.java
+++ src/lanSimulation/Network.java
@@ -20,6 +20,7 @@
 package lanSimulation;
 
 import lanSimulation.internals.*;
+
 import java.util.Hashtable;
 import java.util.Enumeration;
 import java.io.*;
@@ -282,22 +283,14 @@ Therefore #receiver sends a packet across the token ring network, until either
 							endPos = document.message_.indexOf(".", startPos + 6);
 							if (endPos < 0) {endPos = document.message_.length();};
 							title = document.message_.substring(startPos + 6, endPos);};
-							report.write("\tAccounting -- author = '");
-							report.write(author);
-							report.write("' -- title = '");
-							report.write(title);
-							report.write("'\n");
+							introducirDatos(report, author, title);
 							report.write(">>> Postscript job delivered.\n\n");
 							report.flush();
 				} else {
 					title = "ASCII DOCUMENT";
 					if (document.message_.length() >= 16) {
 						author = document.message_.substring(8, 16);};
-						report.write("\tAccounting -- author = '");
-						report.write(author);
-						report.write("' -- title = '");
-						report.write(title);
-						report.write("'\n");
+						introducirDatos(report, author, title);
 						report.write(">>> ASCII Print job delivered.\n\n");
 						report.flush();
 				};
@@ -314,6 +307,15 @@ Therefore #receiver sends a packet across the token ring network, until either
 			};
 			return false;
 		}
+	}
+
+	private void introducirDatos(Writer report, String author, String title)
+			throws IOException {
+		report.write("\tAccounting -- author = '");
+		report.write(author);
+		report.write("' -- title = '");
+		report.write(title);
+		report.write("'\n");
 	}
 
 	/**
